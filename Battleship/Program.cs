95a4949afa9dc18101a5d91cbc9affbf26ae6ea8
Andrei Astakhov aka Progmech <andrei.i.astakhov@gmail.com>
2025-08-04 21:07:23 +0300

    Добавление меню

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.WriteLine("\n1. Новая игра");
+    Console.WriteLine("2. Загрузить игру");
+    Console.WriteLine("3. Сохранить игру");
+    Console.WriteLine("0. Выход\n");
+    Console.WriteLine("Ваш выбор:");
+
+    string userInput = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(userInput)
+    || !int.TryParse(userInput, out int userChoice)
+    || userChoice < 0
+    || userChoice > 3)
+    {
+        Console.WriteLine("Неправильный ввод! Введите число от 0 до 3.");
+        continue;
+    }
+
+    Console.WriteLine("Вы молодец!");
+    break;
+}
