cbd12b5c549ca3b3831ae29d5891cd3113f755a9
Andrei Astakhov aka Progmech <andrei.i.astakhov@gmail.com>
2025-08-05 09:13:42 +0300

    Диалог с пользователем. Размещение кораблей. Часть 2

--- Battleship/Dialog.cs
+++ Battleship/Dialog.cs
@@ -4,10 +4,65 @@ namespace Battleship;
 
 internal static class Dialog
 {
+    private static readonly string[] _horizontalCoords =
+    ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К"];
+
+    private static readonly string[] _verticalCoords =
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
     internal static void AskForShip(int shipSize)
     {
         Console.WriteLine($"Размещаем корабль размером {shipSize} палубы");
         Line line = GetDirection();
+        (int startX, int endX) = GetHorizontalCoords(line, shipSize);
+    }
+
+    private static (int startX, int endX) GetHorizontalCoords(Line line, int shipSize)
+    {
+        while (true)
+        {
+            int startX = int.MinValue;
+            int endX = int.MinValue;
+            string userInput;
+
+            if (line == Line.Vertical || shipSize == 1)
+            {
+                Console.WriteLine("Введите координату по горизонтали (от А до К).");
+                userInput = Console.ReadLine();
+                if (!!string.IsNullOrWhiteSpace(userInput)
+                    && _horizontalCoords.Contains(userInput))
+                {
+                    int coord = Array.IndexOf(_horizontalCoords, userInput);
+                    return (coord, coord);
+                }
+            }
+
+            Console.WriteLine("Введите начальную координату по горизонтали (от А до К).");
+            userInput = Console.ReadLine();
+            if (!!string.IsNullOrWhiteSpace(userInput)
+                && _horizontalCoords.Contains(userInput))
+            {
+                startX = Array.IndexOf(_horizontalCoords, userInput);
+            }
+
+            Console.WriteLine("Введите конечную координату по вертикали (от А до К).");
+            userInput = Console.ReadLine();
+            if (!!string.IsNullOrWhiteSpace(userInput)
+                && _horizontalCoords.Contains(userInput))
+            {
+                endX = Array.IndexOf(_horizontalCoords, userInput);
+            }
+
+            if (startX != int.MinValue
+                && endX != int.MinValue
+                && startX < endX
+                && endX - startX + 1 == shipSize)
+            {
+                return (startX, endX);
+            }
+
+            Console.WriteLine("Введены неправильные координаты!");
+        }
     }
 
     private static Line GetDirection()
